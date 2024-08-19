f26bcf4046e48b18c008a668dff1c016e2a414bd
Aleksandr Aleksandrov <aaleksandrov.cy@gmail.com>
2024-08-19 17:29:53 +0300

    WIP: update project name

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -27,6 +27,6 @@ import (
 // Run e2e tests using the Ginkgo runner.
 func TestE2E(t *testing.T) {
 	RegisterFailHandler(Fail)
-	_, _ = fmt.Fprintf(GinkgoWriter, "Starting vector-operator-migration suite\n")
+	_, _ = fmt.Fprintf(GinkgoWriter, "Starting vector-operator suite\n")
 	RunSpecs(t, "e2e suite")
 }
--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -27,7 +27,7 @@ import (
 	"github.com/kaasops/vector-operator/test/utils"
 )
 
-const namespace = "vector-operator-migration-system"
+const namespace = "vector-operator-system"
 
 var _ = Describe("controller", Ordered, func() {
 	BeforeAll(func() {
@@ -60,7 +60,7 @@ var _ = Describe("controller", Ordered, func() {
 			var err error
 
 			// projectimage stores the name of the image used in the example
-			var projectimage = "example.com/vector-operator-migration:v0.0.1"
+			var projectimage = "example.com/vector-operator:v0.0.1"
 
 			By("building the manager(Operator) image")
 			cmd := exec.Command("make", "docker-build", fmt.Sprintf("IMG=%s", projectimage))
